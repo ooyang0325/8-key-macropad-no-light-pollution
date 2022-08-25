503c1c5ecfe9b611967c09a4bde1e77b0db61e01
light655 <hcchen270@gmail.com>
2022-08-25 08:16:52 +0800

    Testing light pollution,

--- source/light-pollution.py
+++ source/light-pollution.py
@@ -3,26 +3,29 @@ import rp2pio
 import board
 import microcontroller
 import adafruit_pioasm
+import array
 
 
 program1 = """
-.program myWS2812
 .side_set 1
 
 .wrap_target
-pull
-set y 24
+pull                    side 0
+set y 23                side 0
 bit_start:
-    out x 1             side 1  [2]
-    jmp !x zero
+    out x, 1            side 1  [2]
+    jmp !x zero         side 1
 one:
-    nop                         [4]
+    nop                 side 1  [4]
     jmp y-- bit_start   side 0  [6]
+    jmp end             side 0
 zero:
+;    nop                 side 0  [7]
+;    jmp y-- bit_start   side 0  [3]
     jmp y-- bit_start   side 0  [11]
 end:
-    in null 32
-    push
+    mov isr y           side 0
+    push                side 0
 .wrap
 """
 
@@ -30,36 +33,31 @@ program2 = """
 .program myWS2812
 .side_set 1
 
-.wrap_target
 pull
-set y 24
-mov isr osr
+out isr 2
 push
-.wrap
 """
 
-assembled = adafruit_pioasm.assemble(program2)
-rgbPin = microcontroller.pin.GPIO2
+assembled = adafruit_pioasm.assemble(program1)
+rgbPin = board.GP16
 
 sm = rp2pio.StateMachine(
-    assembled, 
+    assembled,
     frequency=12800000,
     first_sideset_pin=rgbPin,
     out_shift_right=False,
+    initial_sideset_pin_state=0,
 )
 
-R = 0
-G = 0
-B = 0
+R = 255
+G = 255
+B = 255
 
-value = bytearray(b"\x00\x00\x00\x00")
-value[0] = G
-value[1] = R
-value[2] = B
-# print(value)
+value = array.array('L', [(G << 24) + (R << 16) + (B << 8)])
+print(value)
 sm.clear_rxfifo()
 sm.write(value)
 print('Wrote some data.')
-# print(sm.frequency)
+time.sleep(0.1)
 sm.readinto(value)
 print(value)
